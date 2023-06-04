42a6303055d9c3cb3416f31a86aa45441ae570cf
Mohit negi <mohitneg377@gmail.com>
2023-06-04 22:31:47 +0530

    Get request with Api testing collection for get

--- testServer.js
+++ testServer.js
@@ -11,19 +11,67 @@ app.use((req, res, next) => {
   next();
 });
 
+let {userData} = require('./userData')
+
 // Routes
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-// Routes
-app.get('/get/usernamebyid/:id', (req, res) => {
-    const userId = req.params.id;
-    // Perform logic to retrieve the username based on the userId
-  
-    // Example response
-    const username = 'John Doe';
-    res.send(`Username for ID ${userId}: ${username}`);
-  });
+//filter users by location
+app.get('/users',(req,res)=>{
+  let city= req.query.city;
+  let usersByLocation = []
+  let cityArr = [];
+  if(city){
+     cityArr= city.split(",");
+     for (const user of userData) {
+       if (cityArr.find((city) => city === user.address.city)) {
+         usersByLocation.push(user);
+       }
+     }
+  }
+  if (usersByLocation.length > 0) {
+    res.send(usersByLocation);
+  } else {
+    res.status(400).send("User not found");
+  }
+})
+//sending usersData
+app.get('/users',(req,res)=>{
+  res.send(userData);
+})
+//sending user data by id
+app.get('/users/:id',(req,res)=>{
+  let id = +req.params.id;
+  let user = userData.find((user)=>user.id === id)
+  // Start the server
+  if(user){
+  res.send(user)
+  }else{
+    res.status(400).send("User Not Found")
+  }
+})
+app.get('/users/:id/address',(req,res)=>{
+  let id = +req.params.id;
+  let user = userData.find((res) => res.id === id)
+  if(user){
+    (user.address) ? res.send(user.address) : res.send("Data not available")
+  }else{
+    res.send(400).send("User Not Found")
+  }
+})
+//get user by userName
+app.get('/users/:username',(req,res)=>{
+  let username = +req.params.username;
+  let user = userData.find((res) => res.username === username)
+  console.log(user)
+  if(user){
+    res.send(user)
+  }else{
+    res.send(400).send("User Not Found")
+  }
+})
 
 // Start the server
 app.listen(port, () => {
